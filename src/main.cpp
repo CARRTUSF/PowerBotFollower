ff5845302a302724cfdcccd169d4313a6a398d9c
Andoni Aguirrezabal <aaguirrezabal@live.com>
2014-08-25 15:26:40 -0400

    Added the Marker Code and Socket Code
    Signed-off-by: Andoni Aguirrezabal <aaguirrezabal@live.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "aruco/aruco.h"
+#include "SockStream.h"
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -13,36 +15,63 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    VideoCapture camCap(0);
+    cv::VideoCapture camCap(1);
 
     aruco::MarkerDetector MDetector;
     vector<aruco::Marker> markers;
     cv::Point2f markerCenter;
-
+    cv::namedWindow("CAMERA",CV_WINDOW_AUTOSIZE);
+    
+    sending_udpsocket clientSocket("192.168.0.104:3251");
+    sockstream networkOut(clientSocket);
+    receiving_udpsocket serverSocket("0.0.0.0:3250");
+    sockstream networkIn(serverSocket);
+    string inputBuffer;
+    
     if(!camCap.isOpened()) {
         cout << "Cannot open the web camera" << endl;
         return -1;
     }
 
+    if(!networkOut.is_open()) {
+        cout << "Could not open the socket for sending" << endl;
+        return -1;
+    }
+
     double imgWidth = camCap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
     double imgHeight = camCap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-
+    
     while(true) {
         cv::Mat newFrame;
+        getline(networkIn, inputBuffer);
 
-        if(!camCap.read(newFrame)) {
-            //PASS
-        } else {
-            //MARKER DETECTOR
-            //SEND INFORMATION
-            if(cv::waitKey(50) == 27) {//wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
-                cout << "ESC Key Pressed, exiting..." << endl;
-                break;
+        if (inputBuffer.compare("REQ") == 0) {
+            if(!camCap.read(newFrame)) {
+                cout << "ERROR: Frame Dropped" << endl;
+                networkOut << "NOM" << endl;
+            } else {
+                cv::imshow("CAMERA", newFrame);
+                MDetector.detect(newFrame, markers);
+
+                if(markers.size() > 0) {
+                    double xRel = 0;
+                    
+                    markers[0].draw(newFrame,cv::Scalar(0,0,255),2);
+                    
+                    cv::Point2f centerPosition = markers[0].getCenter();
+                    xRel = (double)((centerPosition.x - (imgWidth/2.0))
+                                    / ((double)imgWidth));
+
+                    cout << markers[0] << endl;
+                    networkOut << xRel << endl;
+                } else {
+                    cout << "No Marker!" << endl;
+                    networkOut << "NOM" << endl;
+                }
+                cv::waitKey(2);
             }
         }
     }
-
-    //SEND STOP
 
     return 0;
 }
